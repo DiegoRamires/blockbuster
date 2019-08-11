f7841fd0f3c89e94215f9f26ca9a2bcf8cafc1d3
DiegoRamires <goncalves.ramires@gmail.com>
2019-08-11 17:39:06 -0300

    create fake data

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>home page</h1>
-    </div>
+    <main className="container">
+      <h1>
+        Main page
+      </h1>
+    </main>
   );
 }
 
