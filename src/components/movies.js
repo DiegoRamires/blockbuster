2a2af3422dffc08549afcde89b4353688600d45b
DiegoRamires <goncalves.ramires@gmail.com>
2019-08-11 18:17:23 -0300

    create delete movie function

--- src/components/movies.js
+++ src/components/movies.js
@@ -5,6 +5,13 @@ class movies extends Component {
   state = {
     movies: getMovies()
   }
+
+  handleDelete = (movie) => {
+    console.log(movie)
+    const movies = this.state.movies.filter(m => m._id !== movie._id)
+    this.setState({ movies })
+  }
+
   render() {
     return (
       <table className="table">
@@ -14,6 +21,7 @@ class movies extends Component {
             <th>Genre</th>
             <th>Stock</th>
             <th>Rate</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
@@ -23,6 +31,14 @@ class movies extends Component {
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
 
